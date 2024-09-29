1d45b05574f5e580a36221b440f33ccf1aaa9fb3
Naoki Kitamura <108145660+nkitamuraQC@users.noreply.github.com>
2024-09-29 17:44:50 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+from setuptools import setup, find_packages
 
+# requirements.txt を読み込む関数
+def load_requirements(file_name):
+    with open(file_name, 'r') as f:
+        return f.read().splitlines()
+
+setup(
+    name='QSCI',
+    version='0.1', 
+    packages=find_packages(),  
+    install_requires=load_requirements('requirements.txt'), 
+    author='Naoki Kitamura',   
+    url='https://github.com/nkitamuraQC/QSCI.git',  
+)
