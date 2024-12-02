ee780d64a7c58adce562c3eea3948124021e33e2
Naoki Kitamura <qclove00@NaokinoMacBook-Pro.local>
2024-12-02 18:48:10 +0900

    format

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from pyscf.fci import direct_spin1, FCI
 import numpy as np
 from pyscf import gto, scf, ao2mo
 
+
 def test_energy():
     norb = 2
     nelec = 2
@@ -21,12 +22,13 @@ def test_energy():
     assert abs(e1 - e2) < 1e-6
     return
 
+
 def test_H2():
     norb = 2
     nelec = 2
     mol = gto.Mole()
-    mol.atom = '''H 0 0 0; H 0 0 1'''
-    mol.basis = 'sto-3g'
+    mol.atom = """H 0 0 0; H 0 0 1"""
+    mol.basis = "sto-3g"
     mol.build()
     mf = scf.RHF(mol)
     mf.kernel()
